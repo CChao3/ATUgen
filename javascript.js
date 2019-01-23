8675ce9db4c39cc31561936d99908ae53ace1c9c
CChao3 <greyae3@gmail.com>
2019-01-23 04:01:57 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,57 @@
+var plots = [
+'The Theft of xz', 'How the xz Lost Their xz', 'Sham xz and xz', 'Carrying the xz Trickster', 'Biting the xz', 'The Calling of Three xz Names',
+'"xz" on the xz', 'The Unjust xz', 'Stealing the xz\'\s xz', 'xzs Eat One Another Up', 'Eating Their Own xzs',  'The xz Tricks the xz into Falling into a xz',
+'The xz Dives into the xz for Reflected xz', 'The xz in Disguise Violates the xz', 'The xz as xz', 'xz in Split xz', 'The xz Pulls xzs Apart So That the xz\'\s Old xz Can Get xz',
+'The xz Overeats in the xz', 'The xz Builds a xz of xz; the xz, of xz', 'The xz on the xz', 'The xz and the xz', 'Curing a Sick xz', 'The xz\'\s xz',
+'The xz Without a xz', 'The xz at xz', 'The xzs Build a xz (Dig xz)', 'xz with a xz', 'The xz Carries the xz', 'xz and the Sour xz',
+'The xz and the xz Invite Each Other',  'The xz Persuades the xz to Crow with Closed xzs',  'xz Among the xzs - The xz and the xz', 'The xz (or xz) and the xzs',
+'xzless xz Tries in Vain to Get xzs to Cut Off xz', 'Mrs. xz\'\s xzs', 'The xz Trapped in the xz xz', 'More Cowardly Than the xz', 'xz of xz and the xz', 'The xz Rides the xz A-Courting', 'Blinding the xz', 
+'The xz of the xz', 'The xz Admires Themself in a xz',  'xz Allows themself to be Tied to an xz for xz',  'Too Cold for xz to Build xz in xz', 'The xz, the xz, and the xz',
+'The xz of a xz', 'The xz in the xz', 'The xz Taken Seriously', 'When the xz was Married', 'The xz as the xz\'\s xz Sings', 'The Old xz Rescues the xz', 'The xz as xz\'\s xz',
+'xz Between Wild xzs and Domestic xzs', 'The Cowardly xzs', 'The xz\'\s xz', 'xz xz Fears xz Because their xzs are of Different xzs', 'Belling the xz', 'The xz and the xz Converse', 'Town xz and Country xz',
+'xzs Choose xz as xz', 'xz Believes that their Crowing makes the xz Rise', 'The Hungry xz Waits in Vain for the xz\'\s xzs (xz) to Fall Off',
+'The xz on the xz', 'The xz Frightened by the xz', 'The First to See the xz', 'xzs Climb on Top of One Another to xz', 'The xz Loses their xz',
+'The xz and the Seven Young xzs', 'The Three Little xz',  'The xz Flees from the xz', 'Outcast xzs Find a New xz',  'xz as False xz to the xz', 'xz Admires their xzs in the xz',
+'xz Surprises xz in xz xz', 'The Filthy xz and the Clean xz',  'The xz\'\s Three xzs', 'xz Lessons for Wild xzs', 'The Gelding of the xz and the Fetching of xz', 'xz Is the xz\'\s xz', 'Learning to Fear xz', 'The Wild xzs on the xz', 
+'Captured Wild xzs Ransom themselves', 'Grateful xzs; Ungrateful xz', 'xz Betrays xz by Pointing', 'The Singing xz',  'The xz in the xz of xzs',
+'The xz in the xz-trap', 'The xz Eats Their Fellow-xz', 'The Three xzs', 'xz and xzs Readjust Span of xz', 'The xz Tricks the xzs',  'The Faithful xz Rashly Killed', 'What the xz Whispered in Their xz',
+'The Rebounding xz', 'The Helpful xz and the xz', 'The xzs\'\s xz', 'The Lean xz Prefers xz to Abundant xz and a xz', 'xz, xz, and xz in xz at xz',
+'xz Threshed a Second Time', 'xz of the Work xzs', 'The Traveling xzs and the Wicked xz', 'The xz Who was So Tired of Their Daily xz', 'The Lying xz', 
+'xz Tries to Caress their xz like a xz', 'The xz of xzs', 'The xz of xz-King', 'The xz-xz and the xz', 'Wedding of the xz and the xz', 'The xz That Wanted to Fly', 'The xz Teaches the xz to Swim', 
+'The xz\'\s Eternal xz', 'The xz Tries to be as Big as a xz', 'The xz Frightened at the xz\'\s xz', 'The Rearing of the xz-xzed and xz-xzed xz', 'The xz (xz) Transports the xz',
+'The xz and the xzs of xz', 'The xz Borrows the xz\'\s xz',  'Imitating xz xzs', 'The xz That Talked Too Much', 'The Keen xz of the xz and the Keen xz of the xz', 
+'The xz Helps the xz Escape from the xz', 'The Officious xz and the xz', 'The xz Enticed out of their xz', 'The xz Pretends to be xz', 'xz xzs',
+'Tame xz and Wild xz', 'The xz Bends the xz', 'Every xz Thinks Their xz Is the Most Beautiful', 'Swimming xz of the xz', 'The xz and the xz Race to xz', 'The xz of the xz and the xz',
+'xz and the xz Tie xzs Together', 'The xz Carries a xz as Large as Themself', 'xz Invites xz to Rest on their xz', 'xz, xz, and xz Shipwrecked',
+'Deceptive xz-of-xz', 'xz Tries to Get a xz\'\s xz', 'xz of the xz and the xzs', 'The xzs and the xzs', 'xz and xz',
+]
+
+var motifs = ['Fish', 'Bear', 'Blood', 'Brain', 'Foot', 'Tree', 'Painting', 'Haycock', 'Partner', 'Entrail', 'Fox', 'Wolf', 'Pit', 'Water', 'Cheese', 'She-Bear', 
+'Shepherd', 'Claw', 'Mountain', 'Ash', 'Mother', 'Berry', 'Cellar', 'House', 'Wood', 'Ice', 'Oath', 'Iron', 'Honey', 'Lion', 'Ass', 'Heart', 'Court',
+'Animal', 'Road', 'Well', 'Raven', 'Crocodile', 'Jackal', 'Grape', 'Crane', 'Cock', 'Eye', 'Peace', 'Flea', 'Suitor', 'Hide', 'Hare', 'Contest', 'Frost',
+'Rabbit', 'Guard', 'Help', 'Weak', 'Stag', 'Spring', 'Safety', 'Winter', 'Mouse', 'Bird', 'Sausage', 'Needle', 'Glove', 'Squirrel', 'Monkey', 'Master',
+'Dog', 'Guest', 'Child', 'Shoemaker', 'War', 'Dueler', 'Cat', 'Conversation', 'Leader', 'Defeat', 'Force', 'Breed', 'Chanticleer', 'Sun', 'King', 'Horse',
+'Lip', 'Scrotum', 'Hay-Wagon', 'Sunrise', 'Prey', 'Kid', 'Pig', 'Wolf-head', 'Home', 'Tiger', 'Friend', 'Cow', 'Goat', 'Horn', 'Hog', 'Precept', 'Music',
+'Salve', 'Farmer', 'Ingratitude', 'World', 'Reward', 'Man', 'Sleigh', 'Peasant', 'Company', 'Saint', 'Musician', 'Lodger', 'Life', 'Ear', 'Bow', 'Snake',
+'Certificate', 'Liberty', 'Food', 'Chain', 'Sheep', 'Duck', 'Peril', 'Sea', 'Straw', 'Rebellion', 'Candle', 'Council', 'Election', 'Willow', 'Wren',
+'Turkey', 'Peacock', 'Tortoise', 'Goose', 'Prayer', 'Titmouse', 'Hawk', 'Snipe', 'Bill', 'Heron', 'Heathcock', 'Passage', 'Nightingale', 'Blindworm', 'Jay', 
+'Cuckoo', 'Skin', 'Sound', 'Parrot', 'Sight', 'Dove', 'Hearing', 'Frog', 'Crow', 'Deer', 'Snare', 'Hole', 'God', 'Plume', 'Hunter', 'Sparrow', 'Match', 'Pike',
+'Land', 'Net', 'Crayfish', 'Rat', 'Ant', 'Load', 'Spider', 'Fly', 'Curtain', 'Bat', 'Diver', 'Thornbush', 'Tug', 'Cricket', 'Voice', 'Debate', 'Belly',
+'Member', 'Coal', 'Bean', 'Wind',
+]
+
+function newTale() {
+	var rp = Math.floor(Math.random() * (plots.length))
+	var tale = plots[rp]
+	var count = tale.match(/xz/g).length
+	var i
+		for (i = 0; i < count; i++){
+			var rm = Math.floor(Math.random() * (motifs.length))
+			var mtale = tale.replace(/xz/, motifs[rm])
+			tale = mtale}
+	document.getElementById('display').innerHTML = mtale
+
+	}
+
+
+
